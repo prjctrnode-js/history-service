8054f1169faca1288bd2074f730b6fa3fb89a94f
andrey Nemodruk <nemodruk.briz.ua>
2021-12-09 23:26:38 +0200

    fix routes

--- src/middlewares/routes/healthRoute.js
+++
@@ -1,8 +0,0 @@
-const Router = require('@koa/router');
-const historyHealth = require('../../controllers/historyHealth')
-
-const router = new Router();
-
-router.get('/history/health', historyHealth);
-
-module.exports = router;
--- src/middlewares/routes/routes.js
+++ src/middlewares/routes/routes.js
@@ -1,6 +1,15 @@
 const combineRouters = require('koa-combine-routers');
-const healthRoute = require('./healthRoute');
-const historyRoute = require('./historyRoute');
+const Router = require('@koa/router');
+const historyHealth = require('../../controllers/historyHealth')
+const getHistory = require('../../controllers/getHistory')
+const createHistory = require('../../controllers/createHistory')
+
+const healthRoute = new Router()
+const historyRoute = new Router()
+
+healthRoute.get('/history/health', historyHealth);
+historyRoute.get('/history', getHistory);
+historyRoute.post('/history', createHistory);
 
 const router = combineRouters(healthRoute, historyRoute);
 
